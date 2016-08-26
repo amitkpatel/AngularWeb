f54e7097b1f7582a0f0dc8f54a275fddc2b0b95a
amitkpatel <amit_sco19@yahoo.com>
2016-08-26 10:10:39 -0400

    Enabled URL for GetAllCampaigns
    Enabled URL for GetAllCampaigns And Added Campaign status for display

--- src/main/webapp/addcontroller.js
+++ src/main/webapp/addcontroller.js
@@ -1,21 +1,66 @@
 var app = angular.module('mainApp',[]);
 
 app.controller('campaignCtrl',function($scope,$http){
+    $scope.showGetAllCampaignsLink=false;
+    $scope.fetchAllCampaignClicked=false;
+    $scope.fetchActiveCampaignClicked=false;
+
+
 
     $scope.fetchCampaign = function(){
-            var partnerId = $scope.partnerId;
+                var partnerId = $scope.partnerId;
 
-            //$http.get('http://127.0.0.1:8080/AngularJsWebApp/database.json')
-            var url = 'http://127.0.0.1:8080/ad/' + partnerId;
-            $http.get(url)
-            .success(function(response){
-                $scope.campaigns = response;
-                $scope.errorObj = '';
-            })
-            .error(function(response){
-                $scope.campaigns='';
-                $scope.errorObj = response;
-            });
+                var url = 'http://127.0.0.1:8080/ad/' + partnerId;
+                $http.get(url)
+                .success(function(response){
+                    if(response.length>0)
+                    {
+                        $scope.getAllURL=response[0].link.hLink;
+                    }
+                    else{
+                        $scope.getAllURL='http://127.0.0.1:8080/ad/getAllCampaigns/' + partnerId;
+                    }
+                    $scope.campaigns = response;
+                    $scope.errorObj = '';
+                    $scope.fetchActiveCampaignClicked=true;
+                    $scope.fetchAllCampaignClicked=false;
+                    $scope.showGetAllCampaignsLink=true;
+                })
+                .error(function(response){
+                    $scope.campaigns='';
+                    $scope.errorObj = response;
+                    if($scope.errorObj.errorMessage == 'No Active Campaign found for Partner Id : '+$scope.partnerId){
+                        $scope.getAllURL='http://127.0.0.1:8080/ad/getAllCampaigns/' + partnerId;
+						$scope.showGetAllCampaignsLink=true;
+                    }
+                    else{
+                        $scope.showGetAllCampaignsLink=false;
+                        $scope.fetchAllCampaignClicked=false;
+                    }
+                    $scope.fetchActiveCampaignClicked=false;
+
+                });
+				$scope.currentDateTime = new Date();
+    }
+
+    $scope.fetchAllCampaigns = function(){
+
+                 $http.get($scope.getAllURL)
+                 .success(function(response){
+                     $scope.fetchAllCampaignClicked=true;
+                     $scope.fetchActiveCampaignClicked=false;
+                     $scope.campaigns = response;
+                     $scope.errorObj = '';
+                 })
+                 .error(function(response){
+                     $scope.campaigns='';
+                     $scope.errorObj = response;
+                     $scope.fetchAllCampaignClicked=false;
+                     $scope.fetchActiveCampaignClicked=false;
+
+                 });
+				 $scope.currentDateTime = new Date();
+                 return false;
     }
 
 
@@ -39,17 +84,19 @@ app.controller('campaignCtrl',function($scope,$http){
                          }
 
             $http.post('http://127.0.0.1:8080/ad/', data, config)
-            .success(function (data) {
+            .success(function (data,headers) {
 
                 $scope.addPartnerId = '';
                 $scope.addDuration='';
                 $scope.addCampaign='';
                 $scope.saveObjMessage='Campaign Added Successfully.';
                 $scope.saveErrorObj = '';
+
             })
             .error(function(response){
                  $scope.saveObjMessage='';
                  $scope.saveErrorObj = response;
+
             });
 
     }
